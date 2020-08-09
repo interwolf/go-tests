325a9997ef69794f2f4d302c6238b9cb4edbaa73
Yiming Zhang <sdiris@gmail.com>
2020-08-09 12:22:21 +0800

    maps done.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,1 +1,68 @@
 package maps
+
+// Dictionary waps map
+type Dictionary map[string]string
+
+const (
+	// ErrorNotFound is a message for no-existence error
+	ErrorNotFound = DictErr("cannot find the word")
+	// ErrorExistence happens when adding existing words
+	ErrorExistence = DictErr("cannot add the word which already exists")
+	// ErrorNotExistence happens when updating non existing words
+	ErrorNotExistence = DictErr("cannot update the word which not exists")
+)
+
+// DictErr implements the error interface
+type DictErr string
+
+func (e DictErr) Error() string {
+	return string(e)
+}
+
+// Search gives the explanation of input word
+func (d Dictionary) Search(word string) (string, error) {
+	var err error
+
+	result, ok := d[word]
+	if !ok {
+		err = ErrorNotFound
+	} else {
+		err = nil
+	}
+
+	return result, err
+}
+
+// Add will add a word to dict
+func (d Dictionary) Add(word, explain string) error {
+	_, exist := d[word]
+	if exist {
+		return ErrorExistence
+	}
+
+	d[word] = explain
+	return nil
+}
+
+// Update will modify the explanation of a word
+func (d Dictionary) Update(word, explain string) error {
+	_, exist := d[word]
+	if !exist {
+		return ErrorNotExistence
+	}
+
+	d[word] = explain
+	return nil
+}
+
+// Delete will delete word from dict
+// word must exist in dict
+func (d Dictionary) Delete(word string) error {
+	_, exist := d[word]
+	if !exist {
+		return ErrorNotExistence
+	}
+
+	delete(d, word)
+	return nil
+}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,1 +1,125 @@
 package maps
+
+import "testing"
+
+func TestDelete(t *testing.T) {
+
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "test"
+		// want := "explanation of test"
+		var err error
+
+		err = dict.Delete(word)
+		assertError(t, err, ErrorNotExistence)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		want := "explanation of test"
+		dict := Dictionary{word: want}
+
+		var err error
+
+		err = dict.Delete(word)
+		assertError(t, err, nil)
+
+		_, err = dict.Search(word)
+		assertError(t, err, ErrorNotFound)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "test"
+		want := "explanation of test"
+		var err error
+
+		err = dict.Update(word, want)
+		assertError(t, err, ErrorNotExistence)
+
+		_, err = dict.Search(word)
+		assertError(t, err, ErrorNotFound)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		want := "explanation of test"
+		dict := Dictionary{word: "aaa"}
+
+		var err error
+
+		err = dict.Update(word, want)
+		assertError(t, err, nil)
+
+		got, _ := dict.Search(word)
+		assertStrings(t, got, want)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dict := Dictionary{}
+		word := "test"
+		want := "explanation of test"
+		var err error
+
+		err = dict.Add(word, want)
+		assertError(t, err, nil)
+
+		got, _ := dict.Search(word)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		want := "explanation of test"
+		dict := Dictionary{word: want}
+
+		var err error
+
+		err = dict.Add(word, "aaa")
+		assertError(t, err, ErrorExistence)
+
+		got, _ := dict.Search(word)
+		assertStrings(t, got, want)
+	})
+}
+
+func TestSearch(t *testing.T) {
+	dict := Dictionary{"test": "explanation of test"}
+
+	t.Run("existing words", func(t *testing.T) {
+		got, _ := dict.Search("test")
+		want := "explanation of test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("not existing words", func(t *testing.T) {
+		_, got := dict.Search("testa")
+
+		assertError(t, got, ErrorNotFound)
+	})
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+// want should not be nil
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != nil && got != want {
+		t.Errorf("got error %q, want error %q", got, want)
+	}
+	if got == nil && got != want {
+		t.Fatalf("expect to get an error %q but get nil", want)
+	}
+}
