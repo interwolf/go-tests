3b415636c7d3453d01e8f86682036f46c2cdfc4a
Yiming Zhang <sdiris@gmail.com>
2020-08-10 11:12:24 +0800

    DefaultSleeper done.

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	// want := "3\n"
@@ -16,5 +17,9 @@ func TestCountdown(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got: %q, want: %q", got, want)
+	}
+
+	if spySleeper.NumCalls != 4 {
+		t.Errorf("expected number of calls: 4, got: %d", spySleeper.NumCalls)
 	}
 }
--- mocking/main.go
+++ mocking/main.go
@@ -7,17 +7,40 @@ import (
 	"time"
 )
 
+// Sleeper is for mocking system sleep
+type Sleeper interface {
+	Sleep()
+}
+
+// SpySleeper records how many calls to its Sleep()
+type SpySleeper struct {
+	NumCalls int
+}
+
+// Sleep of SpySleeper
+func (s *SpySleeper) Sleep() {
+	s.NumCalls++
+}
+
+// DefaultSleeper is the real sleeper
+type DefaultSleeper struct{}
+
+// Sleep of DefaultSleeper call time.Sleep
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(2 * time.Second)
+}
+
 // Countdown prints 3\n 2\n 1\n go!\n
-func Countdown(writer io.Writer) {
+func Countdown(writer io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(writer, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(writer, "Go!")
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
